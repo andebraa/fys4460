8eeec6f3fd0ab0fd124c08a5ae09cb126da7074d
anders <andebraa@uio.no>
2021-01-28 17:29:54 +0100

    attempt at making readfile. lot's of hurdles

--- test_lennard_jones/readfile.py
+++ test_lennard_jones/readfile.py
@@ -5,14 +5,40 @@ path = 'dump.lammpstrj'
 
 def readfile(filename):
     infile = open(filename, 'r')
-    x = infile.readline()
-    while x[:11] != 'ITEM: ATOMS':
-        x= infile.readline()
-    x = x.split()
-    header = x[2:]
-    print(header)
+    file = infile.readlines()
+    num_atoms = int(file[3])
 
-    # for line in infile:
-        # elem = line.split()
+    #extracting header
+    header = file[8].split()
+
+    num_bins = 30 #number of bins
+    num_timesteps = int(np.shape(file)[0]/(num_atoms+9))
+    velocity_values_i = np.zeros(num_atoms)
+    histogram_matrix = np.zeros((num_timesteps, num_bins, num_bins +1)) #the histogram for each timestep
+    timestep = 0
+    i=0
+    while i < int(np.shape(file)[0]):
+        line = file[i]
+        if line[:14] == 'ITEM: TIMESTEP':
+            print('fuck')
+            timestep +=1
+            hist = np.histogram(velocity_values_i)
+            histogram_matrix[timestep,:,:] = *hist
+
+            print(timestep)
+            for i in range(8):
+                i += 1#skipping number of atoms etc
+
+        elem = file[i]
+        elem = line.split()
+        print(elem)
+        velocity_values_i[i - num_atoms*time_step] = np.linalg.norm((float(elem[-1]), float(elem[-2]), float(elem[-3])))
+        i +=1
+
+
+
+
+
+    print(header)
     infile.close()
 readfile(path)
