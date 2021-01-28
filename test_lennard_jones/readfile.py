c99c7c4e170adac56f6ee2bf7cf0aaa9c0ec0454
anders <andebraa@uio.no>
2021-01-28 17:46:50 +0100

    same strange problem as before

--- test_lennard_jones/readfile.py
+++ test_lennard_jones/readfile.py
@@ -14,7 +14,8 @@ def readfile(filename):
     num_bins = 30 #number of bins
     num_timesteps = int(np.shape(file)[0]/(num_atoms+9))
     velocity_values_i = np.zeros(num_atoms)
-    histogram_matrix = np.zeros((num_timesteps, num_bins, num_bins +1)) #the histogram for each timestep
+    #histogram_matrix = np.zeros((num_timesteps, num_bins, num_bins +1)) #the histogram for each timestep
+    histogram_matrix = []
     timestep = 0
     i=0
     while i < int(np.shape(file)[0]):
@@ -22,8 +23,10 @@ def readfile(filename):
         if line[:14] == 'ITEM: TIMESTEP':
             print('fuck')
             timestep +=1
-            hist = np.histogram(velocity_values_i)
-            histogram_matrix[timestep,:,:] = *hist
+            # hist1, hist2 = np.histogram(velocity_values_i, bins=num_bins)
+            # print(np.shape(hist1))
+            # print(np.shape(hist2))
+            histogram_matrix.append(np.histogram(velocity_values_i, bins=num_bins))
 
             print(timestep)
             for i in range(8):
