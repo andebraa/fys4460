1bc21ab6dd507d285bbd39078d5ba8248abdca97
anders <andebraa@uio.no>
2021-01-31 13:47:11 +0100

    still has issues

--- test_lennard_jones/readfile.py
+++ test_lennard_jones/readfile.py
@@ -14,11 +14,15 @@ def readfile(filename):
     num_bins = 30 #number of bins
     num_timesteps = int(np.shape(file)[0]/(num_atoms+9))
     velocity_values_i = np.zeros(num_atoms)
+    print(num_atoms)
     #histogram_matrix = np.zeros((num_timesteps, num_bins, num_bins +1)) #the histogram for each timestep
     histogram_matrix = []
     timestep = 0
-    i=10
-    while i < int(np.shape(file)[0]):
+    i=9
+    j=0
+    size = int(np.shape(file)[0])
+    print(size)
+    while i <= size:
         line = file[i]
         if line[:14] == 'ITEM: TIMESTEP':
             print('fuck')
@@ -29,10 +33,10 @@ def readfile(filename):
             i += 8
 
         elem = line.split()
-        velocity_values_i[i - num_atoms*timestep] = np.linalg.norm(elem[5:])
+        print(i, timestep, elem[0], j - (num_atoms+9)*timestep)
+        j = (i-9*timestep)
+        velocity_values_i[j - ((num_atoms-9)*timestep)] = np.linalg.norm(elem[5:])
         i +=1
-
-
 
 
 
