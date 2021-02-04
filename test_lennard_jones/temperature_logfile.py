7044067340ef99ef2cc76d3e68b0c8b7a1be11d8
anders <andebraa@uio.no>
2021-02-04 16:52:18 +0100

    tempterature_logfile now reads, step, temp, press, kineng, poteng, toteng, dist. in.lennard now logs these values

--- test_lennard_jones/temperature_logfile.py
+++ test_lennard_jones/temperature_logfile.py
@@ -11,49 +11,86 @@ def readfile(filename):
     file = infile.readlines()
 
     i=0
+
     size = int(np.shape(file)[0])
-    print(size)
     step_num = 0
-    print(file[37][:3])
+    step_size = 0
+
     #skipping preamble, as well as extracting number of timesteps
     while i < 100: #assume preamble of log file is not more than 100 lines
         line = file[i]
-        if line[:35] != "Step Temp E_pair E_mol TotEng Press":
+        if line[:4] == "Step":
             i+=1
+            print('check')
+            break
+        else:
+            i+=1
+            print(line[3:])
             if line[:3] == 'run':
+
                 step_num = int(line[3:])
                 print('cunt')
-        elif line[:35] == "Step Temp E_pair E_mol TotEng Press":
-            i+=1
-            break
-    print(i)
+                print(step_num)
+            if line[:7] == 'thermo ':
+                print('twat')
+                step_size = int(line[6:])
+
+    num_lines = int(step_num/step_size) #number of lines of actual data
+    print(num_lines)
     print(step_num)
-    num_lines = int(step_num/100) #number of lines of actual data
+    print(step_size)
+    print(' ')
     step = np.zeros(num_lines+1)
     temp = np.zeros(num_lines+1)
+    press = np.zeros(num_lines+1)
+    kineng = np.zeros(num_lines+1)
+    poteng = np.zeros(num_lines+1)
+    toteng = np.zeros(num_lines+1)
+    dist = np.zeros(num_lines+1)
     j = 0
     timestep = 0
     while timestep <= num_lines: #assumes log is written every 100 timesteps
         line = file[i]
-        step_, temp_, E_pair, E_mol, TotEng, Press = line.split()
-        step_, temp_, E_pair, E_mol, TotEng, Press = int(step_), float(temp_), \
-                                                   float(E_pair), float(E_mol),\
-                                                   float(TotEng), float(Press)
-        print(step_, temp_, E_pair, E_mol, TotEng, Press)
+        step_, temp_, kineng_, poteng_, toteng_, press_, dist_= line.split()
+        step_, temp_, kineng_, poteng_, toteng_, press_, dist_ = int(step_), \
+                                                                float(temp_), \
+                                                                float(kineng_),\
+                                                                float(poteng_),\
+                                                                float(toteng_),\
+                                                                float(press_),\
+                                                                float(dist_)
+        #print(step_, temp_, kineng_, poteng_, toteng_, press_, dist_)
         step[j] = step_
         temp[j] = temp_
-
+        press[j] = press_
+        kineng[j] = kineng_
+        poteng[j] = poteng_
+        toteng[j] = toteng_
+        dist[j] = dist_
+        print(step_, dist_)
 
         i +=1
         timestep += 1
         j +=1
 
     infile.close()
-    return step, temp, E_pair, E_mol, TotEng, Press
+    return step, temp, press, kineng, poteng, toteng, dist
 
 
 
 
-step, temp, E_pair, E_mol, TotEng, Press = histogram_matrix= readfile(path)
+step, temp, press, kineng, poteng, toteng, dist  = histogram_matrix= readfile(path)
 plt.plot(step, temp)
+plt.xlabel('step')
+plt.ylabel('tempterautre [Lennard Jones]')
+plt.show()
+plt.plot(step, dist)
+plt.xlabel('step')
+plt.ylabel('temp [lennar jones]')
+plt.show()
+
+plt.plot(step, kineng, label ='kineng')
+plt.plot(step, poteng, label ='poteng')
+plt.plot(step, toteng, label ='toteng')
+plt.legend()
 plt.show()
