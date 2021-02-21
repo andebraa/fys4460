a1ecda5f51b4190a38684975ca13778160d98f63
anders <andebraa@uio.no>
2021-02-21 14:06:58 +0100

    issues with reading the file. check indexing

--- lj_variable_temp/log_distance_histogram.py
+++ lj_variable_temp/log_distance_histogram.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.special as ss
 
 path = 'dump.lammpstrj'
 
@@ -13,12 +14,11 @@ def readfile(filename):
 
     num_bins = 30 #number of bins
     num_timesteps = int(np.shape(file)[0]/(num_atoms+9))
-    velocity_values_i = np.zeros(num_atoms)
-    particle_pos = np.zeros(num_atoms, 3) #x,y,z
-    distance_values = np.zeros(num_atoms)
+    particle_pos = np.zeros((num_atoms, 3)) #x,y,z
+    combinations = int((num_atoms*(num_atoms-1))/2)
+    distance_values = np.zeros(combinations)
     print(num_atoms)
-    #histogram_matrix = np.zeros((num_timesteps, num_bins, num_bins +1)) #the histogram for each timestep
-    histogram_matrix = [] #
+
     timestep = 0
     i=9 #running index of each row of the file
     j=0 #actual particle index
@@ -28,23 +28,23 @@ def readfile(filename):
         line = file[i]
         if line[:14] == 'ITEM: TIMESTEP':
             timestep +=1
-            histogram_matrix.append(np.histogram(velocity_values_i, bins=num_bins))
+
             i += 8
+            line = file[i]
 
         elem = line.split() #ITEM: ATOMS id type x y z vx vy vz
-        #print(i, timestep, elem[0], j - (num_atoms+9)*timestep)
+        print(i, timestep, elem[0], j - (num_atoms+9)*timestep)
         j = (i-(9+ 9*timestep))
         particle_pos[j - ((num_atoms)*timestep)] = elem[2:5] #stores the x,y,z distance of particle j
-        velocity_values_i[j - ((num_atoms)*timestep)] = np.linalg.norm(elem[5:])
         i +=1
 
-    for j in range(Natoms):
+    for j in range(num_atoms):
         #thesepos stores all the positions
-        jpos = thesepos[j]
+        jpos = particle_pos[j]
         for k in range(j+1, natoms):
-            dist = jpos = thesepos[k]
+            distance_values = np.linalg.norm((jpos,thesepos[k]), axis=0)
     infile.close()
-    return histogram_matrix
+    return distance_values
 
 # def maxwell(x):
 #     return 600*np.sqrt(2/np.pi)*x**2 * np.exp((-(1.5*x)**2)/2)
