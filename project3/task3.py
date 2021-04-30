eccbff95e65971c90511a9a11ffe3508e6c1c479
anders <>
2021-04-30 17:31:47 +0200

    i'm a dumb fuck

--- project3/task3.py
+++ project3/task3.py
@@ -9,8 +9,6 @@ def matrix_gen(L, p):
     mask = np.logical_not(mask)
     M[mask] = 0
     return M
-M = matrix_gen(10,0.1)
-# print(M)
 
 def find_clusters(array):
     clustered = np.empty_like(array)
@@ -24,29 +22,32 @@ def find_clusters(array):
     return clustered, cluster_count
 
 clustered, count = find_clusters(M)
-# print(clustered)
-# print(count)
+print(clustered)
+print(count)
 
 def spanning_check(clustered):
     spanning=False
+    spanning_cluster = 0
     while not spanning:
-        for i in range(len(clustered[0,:])):
-            for j in range(len(clustered[-1,:])):
-                if clustered[0,i] == clustered[-1,j] and M[0,i] != 0:
-                    spanning_cluster = M[i,j]
-                    print(spanning_cluster)
+        for i in range(len(clustered[:,0])):
+            for j in range(len(clustered[:,-1])):
+                if clustered[i,0] == clustered[j,-1] and M[0,i] != 0:
+                    print('cunt')
+                    spanning_cluster = clustered[i,j]
                     spanning=True
-                    return spanning_cluster
 
-        spanning = False
-        print(spanning)
-        print(spanning_cluster, spanning)
-        return (spanning_cluster, spanning)
+                    spanning_cluster_matrix = np.where(clustered==spanning_cluster)
+                    #M[spanning_cluster_matrix] = 99
+                    #plt.imshow(clustered)
+                    return spanning_cluster, spanning
+
+
+    return (spanning_cluster, spanning)
 
 spanning_cluster, spanning = spanning_check(clustered)
-if spanning:
-    spanning_cluster_matrix = np.where(clustered==spanning_cluster)
-    #M[spanning_cluster_matrix] = 99
-    #plt.imshow(clustered)
-    plt.imshow(M)
-    plt.show()
+
+def main():
+    L = 100
+    spanning = False
+    while not spanning:
+main()
