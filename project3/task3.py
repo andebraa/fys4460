fa17f6a49278b9db8b9c849266639975ea9fc1cf
andebraa <andebraa@uio.no>
2021-05-02 16:30:10 +0200

    started P vs L plot

--- project3/task3.py
+++ project3/task3.py
@@ -27,11 +27,12 @@ def find_clusters(array):
 def spanning_check(clustered,M):
     spanning=False
     spanning_cluster = 0
+    Ms = 0 #the mass of the spanning cluster
     while not spanning:
         for i in range(len(clustered[:,0])):
             for j in range(len(clustered[:,-1])):
 
-                if clustered[i,0] == clustered[j,-1] and M[i,0] != 0:
+                if clustered[i,0] == clustered[j,-1] and M[i,0] != 0: #checks both right and left edge, if the same cluster appears it's bounding
                     empt = np.zeros_like(M)
                     print('cunt')
                     spanning_cluster = clustered[i,0]
@@ -39,29 +40,34 @@ def spanning_check(clustered,M):
 
                     spanning_cluster_matrix = np.where(clustered==spanning_cluster)
                     clustered[spanning_cluster_matrix] = np.max(clustered)+16
-                    empt[spanning_cluster_matrix] = 2
+                    empt[spanning_cluster_matrix] = 1
+                    Ms = np.sum(empt)
                     norm_inst = matplotlib.colors.Normalize(vmin=None, vmax=None, clip=False)
                     plt.subplot(2,1,1)
                     plt.imshow(clustered, norm=norm_inst) #,norm=norm_inst)
                     plt.subplot(2,1,2)
                     plt.imshow(empt)
                     plt.show()
-                    return spanning_cluster, spanning
+                    return spanning_cluster, spanning, Ms
 
 
-        return (spanning_cluster, spanning)
+        return spanning_cluster, spanning, Ms
 
 
 def main():
-    L = 200
-    spanning = False
-    p = 0.55
-    while not spanning:
-        print(p)
-        m = matrix_gen(L,p)
-        clustered=find_clusters(m)
-        spanning_cluster, spanning = spanning_check(clustered,m)
-        p+=0.001
-    print(m)
-    print(spanning_cluster, spanning)
+    L_list = [2,4]#,8,16,32,64,128]
+    p_arr = np.linspace(0.58, 0.6, 10)
+    P_list = np.zeros(len(L_list),len(p_arr))
+    for i,L in enumerate(L_list):
+        spanning = False
+        p = 0.55
+        for j,p in p_arr:
+            print(p)
+            m = matrix_gen(L,p)
+            clustered=find_clusters(m)
+            spanning_cluster, spanning, Ms = spanning_check(clustered,m)
+            p+=0.001
+            if spanning:
+                p[i,j] = Ms/L**2
+    plt.plot()
 main()
