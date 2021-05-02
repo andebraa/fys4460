07cd6aa232b1d0e4254063eb5bbb2103c1b9c2a2
anders <>
2021-05-02 13:02:52 +0200

    works now

--- project3/task3.py
+++ project3/task3.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
+
 def matrix_gen(L, p):
     M = np.random.uniform(0,1,(L,L))
-    mask = M>p
+    mask = M<p
     M[mask] = 1
     mask = np.logical_not(mask)
     M[mask] = 0
@@ -19,35 +21,42 @@ def find_clusters(array):
         for k in range(1, label_count + 1):
             clustered[labelling == k] = cluster_count
             cluster_count += 1
-    return clustered, cluster_count
+    return clustered#, cluster_count
 
-clustered, count = find_clusters(M)
-print(clustered)
-print(count)
 
-def spanning_check(clustered):
+def spanning_check(clustered,M):
     spanning=False
     spanning_cluster = 0
     while not spanning:
         for i in range(len(clustered[:,0])):
             for j in range(len(clustered[:,-1])):
-                if clustered[i,0] == clustered[j,-1] and M[0,i] != 0:
+
+                if clustered[i,0] == clustered[j,-1] and M[i,0] != 0:
                     print('cunt')
                     spanning_cluster = clustered[i,j]
                     spanning=True
 
                     spanning_cluster_matrix = np.where(clustered==spanning_cluster)
-                    #M[spanning_cluster_matrix] = 99
-                    #plt.imshow(clustered)
+                    clustered[spanning_cluster_matrix] = np.max(clustered)+10
+                    #map = matplotlib.colors.Colormap('name')
+                    plt.imshow(clustered) #, cmap=map)
+                    plt.show()
                     return spanning_cluster, spanning
 
 
-    return (spanning_cluster, spanning)
+        return (spanning_cluster, spanning)
 
-spanning_cluster, spanning = spanning_check(clustered)
 
 def main():
     L = 100
     spanning = False
+    p = 0.5
     while not spanning:
+        print(p)
+        m = matrix_gen(L,p)
+        clustered=find_clusters(m)
+        spanning_cluster, spanning = spanning_check(clustered,m)
+        p+=0.001
+    print(m)
+    print(spanning_cluster, spanning)
 main()
