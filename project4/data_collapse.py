de5d1f62f996e3f7a24454d42a1aebd28bb076df
andebraa <andebraa@uio.no>
2021-06-08 15:37:18 +0200

    backup

---
+++ project4/data_collapse.py
@@ -0,0 +1,205 @@
+import numpy as np
+import matplotlib
+import pylab
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from scipy.ndimage import measurements
+from tqdm import tqdm
+np.random.seed(80085)
+
+
+def matrix_gen(L, p):
+    M = np.random.uniform(0,1,(L,L))
+    mask = M<p
+    M[mask] = 1
+    mask = np.logical_not(mask)
+    M[mask] = 0
+    return M
+
+
+def find_clusters(array):
+    clustered = np.empty_like(array)
+    unique_vals = np.unique(array)
+    cluster_count = 0
+    for val in unique_vals:
+        labelling, label_count = ndimage.label(array == val)
+        for k in range(1, label_count + 1):
+            clustered[labelling == k] = cluster_count
+            cluster_count += 1
+    return clustered#, cluster_count
+
+
+
+def spanning_check(clustered,M):
+    spanning=False
+    spanning_cluster = 0
+    Ms = 0 #the mass of the spanning cluster
+    while not spanning:
+        for i in range(len(clustered[:,0])):
+            for j in range(len(clustered[:,-1])):
+
+                if clustered[i,0] == clustered[j,-1] and M[i,0] != 0: #checks both right and left edge, if the same cluster appears it's bounding
+                    empt = np.zeros_like(M)
+                    spanning_cluster = clustered[i,0]
+                    spanning=True
+
+                    spanning_cluster_matrix = np.where(clustered==spanning_cluster)
+                    clustered[spanning_cluster_matrix] = np.max(clustered)+16
+                    empt[spanning_cluster_matrix] = 1
+                    Ms = np.sum(empt)
+                    # norm_inst = matplotlib.colors.Normalize(vmin=None, vmax=None, clip=False)
+                    # plt.subplot(2,1,1)
+                    # plt.imshow(clustered, norm=norm_inst) #,norm=norm_inst)
+                    # plt.subplot(2,1,2)
+                    # plt.imshow(empt)
+                    # plt.show()
+                    return spanning_cluster, spanning, Ms
+
+
+        return spanning_cluster, spanning, Ms
+
+
+def data_collapse():
+    """
+    attempt at re-creating fig 6.2
+    """
+
+    L_list = [25,50,100,200]
+    p_pc = np.linspace(0, 1, 20) #supposed to be p-pc
+    P_list = np.zeros((len(L_list),len(p_pc)))
+    spanns = np.zeros((len(L_list),len(p_pc)))
+    iterations = 10
+    for i,L in enumerate(L_list):
+        for iter in range(iterations): #to get an average for each variable
+            spanning = False
+            for j,p in enumerate(p_pc):
+                m = matrix_gen(L,p)
+                #clustered,_ = measurements.label(m)
+                clustered = find_clusters(m)
+                spanning_cluster, spanning, Ms = spanning_check(clustered,m)
+                if spanning:
+                    P_list[i,j] += Ms/L**2
+                    spanns[i,j] +=1
+        P_list[i,:]/=iterations
+        spanns[i,:]/=iterations
+        D_d = 0.11
+        mu = 4/3
+
+        plt.plot(p_pc*L**(mu), P_list[i,:]*L**(-D_d), label=f'{L}')
+
+
+    plt.xlabel(r'$(p-p_c)L^x$')
+    plt.ylabel(r'y')
+    plt.legend()
+    plt.show()
+
+
+def p_vs_L():
+    """
+    for plotting P(pc,L) to show that P goes to zero for L -> infty
+    (see fig 6.1)
+    """
+    L_list = [2,4,8,16,32,64,128]
+    #p_arr = 0.5927
+    p = 0.5927
+    P_list = np.zeros(len(L_list))
+    spanns = np.zeros(len(L_list))
+    iterations = 1000
+    for i,L in enumerate(L_list):
+        for iter in range(iterations): #to get an average for each variable
+            spanning = False
+            m = matrix_gen(L,p)
+            #clustered,_ = measurements.label(m)
+            clustered = find_clusters(m)
+            spanning_cluster, spanning, Ms = spanning_check(clustered,m)
+            if spanning:
+                P_list[i] += Ms/L**2
+                spanns[i] +=1
+        P_list[i]/=iterations
+        spanns[i]/=iterations
+    plt.plot(L_list, P_list, '-o')
+    plt.xlabel('L')
+    plt.ylabel('P(pc)')
+    plt.show()
+
+def L():
+    """
+    Generates a P and pi plot vs p for a single size L
+    """
+    L = 50
+    p_arr = np.linspace(0.1, 0.9, 50)
+    P_list = np.zeros(len(p_arr))
+    spanns = np.zeros(len(p_arr))
+    iterations = 10
+
+    for iter in range(iterations): #to get an average for each variable
+        spanning = False
+        for j,p in enumerate(p_arr):
+            m = matrix_gen(L,p)
+            #clustered,_ = measurements.label(m)
+            clustered = find_clusters(m)
+            spanning_cluster, spanning, Ms = spanning_check(clustered,m)
+            if spanning:
+                P_list[j] += Ms/L**2
+                spanns[j] +=1
+    P_list/=iterations
+    spanns/=iterations
+    plt.subplot(2,1,1)
+    plt.plot(p_arr, P_list, label=f'{L}')
+    plt.xlabel(r'p')
+    plt.ylabel(r'P(L,p)')
+    plt.legend()
+    plt.subplot(2,1,2)
+    plt.plot(p_arr, spanns, label=f'{L}')
+    plt.xlabel(r'p')
+    plt.ylabel(r"$\Pi(L,p)$")
+    plt.legend()
+
+
+    plt.show()
+
+def clus_num_den(L=200, p=0.5, iterations = 1000, log_base =2, logarit_bins = False):
+
+    iterations = 1000
+    L = 10
+
+    tot_area = pylab.array([])
+    for i in range(iterations):
+        m = matrix_gen(L,p)
+
+        clustered, clustered_number = measurements.label(m)
+        labellist = pylab.arange(clustered_number+1)
+        area = measurements.sum(m, clustered, labellist)
+        tot_area = pylab.append(tot_area, area)
+
+    num, hist = pylab.histogram(tot_area, bins=int(max(tot_area)))
+    s = 0.5*(hist[1:]+hist[:-1])
+    nsp = num/(L*iterations)
+
+    if not logarit_bins:
+        ret = s, nsp
+
+    elif logarit_bins:
+        logamax = pylab.ceil(pylab.log(max(s))/(pylab.log(log_base)))
+        logbins = log_base**pylab.arange(0,logamax) #this generates a^i bins
+        nl, nlbins = pylab.histogram(tot_area, bins=logbins)
+
+        ds = pylab.diff(logbins)
+        bins = 0.5*(logbins[1:] + logbins[:-1])
+        log_hist_normed = nl/(L**2*ds) #M?
+        ret =  bins, log_hist_normed
+
+    return ret
+
+
+data_collapse()
+
+#plot_nsp()
+
+
+# m = matrix_gen(30, 0.6)
+# plt.subplot(2,1,1)
+# plt.imshow(find_clusters(m))
+# plt.subplot(2,1,2)
+# plt.imshow(find_clusters(m))
+# plt.show()
---
+++ project4/test_gpl.py
@@ -0,0 +1,123 @@
+import pylab as pl
+from scipy.ndimage import measurements
+from matplotlib.colors import ListedColormap
+from tqdm import tqdm
+from scipy.sparse import spdiags, dia_matrix, coo_matrix
+from scipy.sparse.linalg import spsolve
+import numpy as np
+import os
+
+def sitetobond(z):
+    #Function to convert the site network z(L,L) into a (L*L,2) bond
+    #network
+    # g [i,0] gives bond perpendicular to direction of flow
+    # g [i,1] gives bond parallel to direction of flow
+    # z [ nx, ny] -> g [nx * ny, 2]
+    nx = pl.size(z, 1-1)
+    ny = pl.size(z, 2-1)
+    N = nx*ny
+    gg_r = pl.zeros((nx,ny)) #first find these
+    gg_d = pl.zeros((nx,ny))
+    gg_r[:,0:ny-1] = z[:,0:ny-1]*z[:,1:ny]
+    gg_r[:,ny-1] = z[:,ny-1]
+
+    gg_d[0:nx-1,:] = z[0:nx-1,:]*z[1:nx,:]
+    gg_d[nx-1,:] = 0
+    #then concatenate gg onto g
+    g = pl.zeros((nx*ny,2))
+    g[:,0] = gg_d.reshape(-1,order='F').T
+    g[:,1] = gg_r.reshape(-1,order='F').T
+    return g
+def FIND_COND(A,X,Y):
+    V_in = 1.
+    V_out = 0.
+    #calls MK_Eqsystem
+    B,C = MK_EQSYSTEM(A,X,Y)
+    #Kirchhoff's equations solve for V
+    V  = spsolve(B,C)
+    #the pressure at the external sites is added
+    #(boundary condition)
+    V = pl.concatenate((V_in*pl.ones(X), V,V_out*pl.ones(X)))
+    #calculate ceff
+    #second-last X elements of V multiplied with second-last elem.of A
+    #these are the second last column of the system
+    #gives the conductivity of the system per row?
+
+    Ceff = pl.dot((V[-1-2*X:-1-X] -V_out).T, A[-1-2*X:-1-X, 1])\
+    /(V_in - V_out)
+    return V,Ceff
+
+def MK_EQSYSTEM(A,X,Y):
+    #total no of internal lattice sites
+    sites = X*(Y-2)
+    #allocate space for the nonzeros uppder diagonals
+    main_diag = pl.zeros(sites)
+    uppder_diag1 = pl.zeros(sites-1)
+    uppder_diag2 = pl.zeros(sites-X)
+    #calculate the nonzero upper diagonals
+    main_diag = A[X:X*(Y-1), 0] + A[X:X*(Y-1),1] +\
+                A[0:X*(Y-2),1] + A[X-1:X*(Y-1)-1,0]
+    upper_diag1 = A[X:X*(Y-1)-1,0]
+    upper_diag2 = A[X:X*(Y-2),1]
+    main_diag[pl.where(main_diag==0)] = 1
+    #constructing B which is symmetric, lower = upper diagonals
+    B = dia_matrix((sites,sites)) #B*u=t
+    B = - spdiags(uppder_diag1, -1, sites, sites)
+    B = B + -spdiags(upper_diag2, -X, sites, sites)
+    B = B+B.T + spdiags(main_diag, 0 , sites, sites )
+    #constructing C
+    C = pl.zeros(sites)
+    # C = dia_matrix ( (sites, 1))
+    C[0:X] = A[0:X, 1]
+    C[-1-X+1:-1] = 0*A[-1-2*X+1:-1-X,1]
+    return B,C
+
+
+
+Lvals = [25,50,100,200,400]
+pVals = pl.logspace(pl.log10(0.58), pl.log10(0.85), 20)
+
+C = pl.zeros((len(pVals),len(Lvals)),float)
+P = pl.zeros((len(pVals),len(Lvals)),float)
+nSamples = 100
+mu = pl.zeros(len(Lvals))
+for iL in tqdm(range(len(Lvals))):
+    L = Lvals[iL]
+    lx = L
+    ly = L
+    for pIndex in tqdm(range(len(pVals))):
+        p = pVals[pIndex]
+        ncount = 0
+        for j in range(nSamples):
+            ncount = 0
+            perc = []
+            while (len(perc)==0):
+                ncount = ncount + 1
+                if (ncount > 1000):
+                    print("Couldn’t make percolation cluster...")
+                    break
+                z=pl.rand(L,L)<p
+                lw,num = measurements.label(z)
+                perc_x = pl.intersect1d(lw[0,:],lw[-1,:])
+                perc = perc_x[pl.where(perc_x > 0)]
+            if len(perc) > 0:
+                zz = pl.asarray((lw == perc[0]))
+                # zz now contains the spanning cluster
+                zzz = zz.T
+                #
+                # Generate bond lattice from this
+                g = sitetobond ( zzz )
+                #
+                # Generate conductivity matrix
+                Pvec, c_eff = FIND_COND(g, lx, ly)
+                C[pIndex,iL] = C[pIndex,iL] + c_eff
+        C[pIndex,iL] = C[pIndex,iL]/nSamples
+
+
+for iL in range(len(Lvals)):
+    L = Lvals[iL]
+    pl.plot(pVals,C[:,iL],label="L="+str(L))
+pl.xlabel(r"$p$")
+pl.ylabel(r"$C$")
+pl.legend()
+pl.show()
